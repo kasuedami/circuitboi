cee963d7a27378b533e50231855b476a18fbfd61
Sebastian Nocke <sjno@gmx.de>
2023-02-20 20:52:16 +0100

    Centering confirmation window

--- src/editor/state.rs
+++ src/editor/state.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 
-use bevy::prelude::{Resource, ResMut, info};
+use bevy::{prelude::{Resource, ResMut, info, Res}, window::Windows};
+use bevy_egui::{EguiContext, egui::{self, Align2}};
 
 #[derive(Default, Resource)]
 pub(crate) struct EditorState {
@@ -31,18 +32,40 @@ impl EditorState {
     }
 }
 
-pub(super) fn handle_editor_state(editor_state: ResMut<EditorState>) {
+pub(super) fn handle_editor_state(
+    editor_state: ResMut<EditorState>,
+    egui_context: ResMut<EguiContext>,
+    windows: Res<Windows>,
+) {
     match editor_state.action {
-        EditorStateAction::Reset => reset(editor_state),
+        EditorStateAction::Reset => reset(editor_state, egui_context, windows),
         EditorStateAction::Save => save(editor_state),
         EditorStateAction::SaveAs => save_as(editor_state),
         _ => (),
     }
 }
 
-fn reset(mut editor_state: ResMut<EditorState>) {
-    editor_state.action = EditorStateAction::None;
-    editor_state.editing_file = None;
+fn reset(
+    mut editor_state: ResMut<EditorState>,
+    mut egui_context: ResMut<EguiContext>,
+    windows: Res<Windows>,
+) {
+
+    let center_width = windows.get_primary().unwrap().width() / 2.0;
+    let center_height = windows.get_primary().unwrap().height() / 2.0;
+
+    egui::Window::new("Confirm reset")
+        .pivot(Align2::CENTER_CENTER)
+        .fixed_pos(egui::pos2(center_width, center_height))
+        .show(egui_context.ctx_mut(), |ui| {
+            if ui.button("Yes").clicked() {
+                editor_state.editing_file = None;
+                editor_state.action = EditorStateAction::None;
+            }
+            if ui.button("No").clicked() {
+                editor_state.action = EditorStateAction::None;
+            }
+    });
 }
 
 fn save(mut editor_state: ResMut<EditorState>) {
